c24752225a2a4ba9a6a72b34a073a10a13c0fb4e
Allen Guo <guosonglin001@gmail.com>
2026-03-03 07:11:58 +0000

    sync

--- scripts/vllm.sh
+++ scripts/vllm.sh
@@ -10,3 +10,12 @@ uv pip install --system vllm-0.16.0-cp38-abi3-manylinux_2_31_x86_64.whl
 
 # optional
 # uv pip install --system "transformers>=5.0.0"
+
+BASE_URL="https://wheels.vllm.ai/nightly/"
+# BASE_URL="https://wheels.vllm.ai/nightly/cu129/vllm"
+# BASE_URL="https://wheels.vllm.ai/nightly/cu130/vllm"
+
+# cu129 eg:
+# curl -O -L https://wheels.vllm.ai/3a6cbf16e27e164e81ac2259fe211cd930adf48f/vllm-0.16.1rc1.dev171%2Bg3a6cbf16e-cp38-abi3-manylinux_2_31_x86_64.whl
+# cu130 eg:
+# curl -O -L https://wheels.vllm.ai/3a6cbf16e27e164e81ac2259fe211cd930adf48f/vllm-0.16.1rc1.dev171%2Bg3a6cbf16e.cu130-cp38-abi3-manylinux_2_35_x86_64.whl
