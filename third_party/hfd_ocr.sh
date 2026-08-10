64c52f05c5203a5c6ef53ded3782b229fcb9e2c8
Allen Guo <guosonglin001@gmail.com>
2026-08-10 22:02:35 +0800

    ocr

--- third_party/hfd_ocr.sh
+++ third_party/hfd_ocr.sh
@@ -7,31 +7,34 @@ export HF_ENDPOINT=https://hf-mirror.com
 
 pushd third_party
 
-hfd.sh zai-org/GLM-OCR
-# https://huggingface.co/zai-org/GLM-OCR
-
-hfd.sh FireRedTeam/FireRed-OCR
-# https://huggingface.co/FireRedTeam/FireRed-OCR
-
-hfd.sh datalab-to/chandra-ocr-2
-# https://huggingface.co/datalab-to/chandra-ocr-2
-
-hfd.sh PaddlePaddle/PaddleOCR-VL-1.6
-# https://huggingface.co/PaddlePaddle/PaddleOCR-VL-1.6
-
 hfd.sh rednote-hilab/dots.mocr
 # https://huggingface.co/rednote-hilab/dots.mocr
 
-hfd.sh rednote-hilab/dots.mocr-svg
-# https://huggingface.co/rednote-hilab/dots.mocr-svg
+hfd.sh baidu/Unlimited-OCR
+# https://huggingface.co/baidu/Unlimited-OCR
 
 hfd.sh tencent/HunyuanOCR
 # https://huggingface.co/tencent/HunyuanOCR
 
-hfd.sh infly/Infinity-Parser2-Pro
-# https://huggingface.co/infly/Infinity-Parser2-Pro
-
 hfd.sh infly/Infinity-Parser2-Flash
 # https://huggingface.co/infly/Infinity-Parser2-Flash
+
+# hfd.sh zai-org/GLM-OCR
+# https://huggingface.co/zai-org/GLM-OCR
+
+# hfd.sh FireRedTeam/FireRed-OCR
+# https://huggingface.co/FireRedTeam/FireRed-OCR
+
+# hfd.sh datalab-to/chandra-ocr-2
+# https://huggingface.co/datalab-to/chandra-ocr-2
+
+# hfd.sh PaddlePaddle/PaddleOCR-VL-1.6
+# https://huggingface.co/PaddlePaddle/PaddleOCR-VL-1.6
+
+# hfd.sh rednote-hilab/dots.mocr-svg
+# https://huggingface.co/rednote-hilab/dots.mocr-svg
+
+# hfd.sh infly/Infinity-Parser2-Pro
+# https://huggingface.co/infly/Infinity-Parser2-Pro
 
 popd
