33a0abe807861310de343a4baf47c547aa3cd11c
Allen Guo <guosonglin001@gmail.com>
2026-08-10 22:17:25 +0800

    3rd

--- third_party/repos.sh
+++ third_party/repos.sh
@@ -3,6 +3,7 @@ pushd third_party
 git clone git@github.com:rednote-hilab/dots.mocr.git
 git clone git@github.com:Tencent-Hunyuan/HunyuanOCR.git
 git clone git@github.com:baidu/Unlimited-OCR.git
+git clone git@github.com:infly-ai/INF-MLLM.git
 
 git clone git@github.com:vllm-project/vllm.git --single-branch --depth 1000
 
